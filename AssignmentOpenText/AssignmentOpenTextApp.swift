58264958bbd70247dd73b4034af44c3eb8b0638f
Mac <vachhani.vibha95@gmail.com>
2023-08-15 17:36:48 -0400

    Initial Commit

---
+++ AssignmentOpenText/AssignmentOpenTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentOpenTextApp.swift
+//  AssignmentOpenText
+//
+//  Created by Mac on 15/08/23.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentOpenTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
